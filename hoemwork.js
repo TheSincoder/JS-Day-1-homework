75bc838449e67322b4646f97a51afac0cc76201b
NobleSincoder <sincode.reeves@gmail.com>
2022-02-17 20:56:27 -0700

    added codewars problems

--- hoemwork.js
+++ hoemwork.js
@@ -24,3 +24,44 @@ function replaceEvens(arr){
 };
 
 console.log(replaceEvens(arr))
+
+// Return Negative
+// # In this simple assignment you are given a number and have to make it negative. 
+// # But maybe the number is already negative?
+
+// python code:
+// def make_negative(x):
+//     return -abs(x)
+
+const n1 = 5;
+const n2 = 7;
+const n3 = -5;
+const n4 = 100;
+
+function makeNegative(x){
+    console.log(-Math.abs(x))
+};
+
+makeNegative(n4)
+
+// Square N Sum
+// # Complete the square sum function so that it squares each number passed into 
+// # it and then sums the results together.
+
+// def square_sum(numbers):
+//     sum = 0
+//     for num in numbers:
+//         sum+= num**2
+//     return sum
+
+let numbers = [5,2]
+
+function squareSum(numbers){
+    let sum = 0;
+    for (let num of numbers){
+        sum+= num**2
+    };
+    return sum
+};
+
+console.log(squareSum(numbers))
